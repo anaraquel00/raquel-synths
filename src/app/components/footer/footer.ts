d5428f643421c70893a947b3febec3e044530743
anaraquel <anaraquel00@gmail.com>
2025-12-09 09:32:32 -0300

    add link footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -9,9 +9,9 @@ import { MatIconModule } from "@angular/material/icon";
 export class Footer {
 openDonation() {
     // Opção A: Alerta Simples
-    alert("💸 O Cofre da RQS está sendo forjado! Em breve você poderá apoiar nossa arte. Obrigado pela intenção! 🖤");
+    //alert("💸 O Cofre da RQS está sendo forjado! Em breve você poderá apoiar nossa arte. Obrigado pela intenção! 🖤");
 
-    // Opção B: Se tiver o link, abre (futuro)
-    // window.open('SEU_LINK_PAYPAL', '_blank');
+    //Opção B: Se tiver o link, abre (futuro)
+     //window.open('https://www.paypal.com/donate/?hosted_button_id=THMR8MPC7XYRU', '_blank');
   }
 }
