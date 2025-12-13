5166eeba8c34a327305a31143d9fc2fa8fd5dc99
anaraquel <anaraquel00@gmail.com>
2025-12-13 18:25:12 -0300

    update contato and home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, DOCUMENT, Inject } from '@angular/core';
 import { TranslationService } from '../../services/translation.service';
 import { CONTACT_DATA, HOME_DATA } from '../../data/app-data';
 import { MatButtonModule } from '@angular/material/button';
-import { ContatoComponent } from '../contato/contato';
 
 @Component({
   selector: 'app-home',
-  imports: [ContatoComponent,
-       MatButtonModule],
+  imports: [MatButtonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-  document: any;
-   constructor(public translate: TranslationService) { }
+
+  constructor(public translate: TranslationService,
+    @Inject(DOCUMENT) private document: Document) { }
+
   get navText() {
     return this.translate.isPt() ? HOME_DATA.pt : HOME_DATA.en;
   }
@@ -23,9 +23,13 @@ export class Home {
   }
 
    scrollTo(elementId: string): void {
+    // Agora ele busca no HTML GLOBAL (app.html)
     const element = this.document.getElementById(elementId);
     if (element) {
+      // O 'block: start' garante que o topo da seção fique no topo da tela
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      console.warn(`ALERTA: Elemento '${elementId}' não encontrado no DOM.`);
     }
   }
 
