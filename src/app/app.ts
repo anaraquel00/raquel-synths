8f67f10e1913a40d0875ef5470b7e74714f81935
anaraquel <anaraquel00@gmail.com>
2025-12-13 18:59:42 -0300

    fix scroll

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 import { Home } from "./pages/home/home";
