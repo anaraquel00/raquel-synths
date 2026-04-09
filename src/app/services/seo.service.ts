d7f98d195fcdd642a259d7eee3ebea14408deb9a
Ana Raquel de Holanda Barros <anaraquel00@gmail.com>
2026-04-09 07:01:16 -0300

    update index & seo.service.ts

--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -23,14 +24,40 @@ export class SeoService {
     this.meta.updateTag({ name: 'twitter:title', content: pageTitle });
     this.meta.updateTag({ name: 'twitter:description', content: pageDesc });
     this.meta.updateTag({ name: 'twitter:image', content: pageImage });
+
+    // 👇 Dispara o protocolo Canônico
+    if (config.url) {
+      this.setCanonicalUrl(config.url);
+    }
   }
 
+ // 🛡️ Módulo de Blindagem de Rota
+  private setCanonicalUrl(url: string) {
+    const head = this.dom.getElementsByTagName('head')[0];
+    let element: HTMLLinkElement | null = this.dom.querySelector(`link[rel='canonical']`);
+
+    // Aqui nós limpamos qualquer parâmetro de caos (?mode=jonah) da URL oficial
+    // Se a URL enviada for /dossier?mode=jonah, o cleanUrl será apenas /dossier
+    const cleanUrl = url.split('?')[0];
+
+    if (!element) {
+      // Se a tag não existe, nós forjamos uma nova
+      element = this.dom.createElement('link') as HTMLLinkElement;
+      element.setAttribute('rel', 'canonical');
+      head.appendChild(element);
+    }
+    // Ancoramos a URL limpa
+    element.setAttribute('href', cleanUrl);
+  }
+  
   // O núcleo da nossa narrativa (Sincronizado com o index.html oficial)
   private defaultTitle = 'RaQuel Synths | Cyberpunk Sagas & Virtual Band';
   private defaultDesc = 'Broklin\'s Tech vs. Jonah\'s Chaos. A Cyberpunk Literary Saga & Musical Experiment. The story has begun.';
   private defaultImage = 'https://raquelsynths.com.br/images/banner-seo-global.jpg';
 
-  constructor(private title: Title, private meta: Meta) { }
+  constructor(private title: Title, 
+              private meta: Meta,
+             @Inject(DOCUMENT) private dom: Document) { }
 
   updateTags(config?: { title?: string, description?: string, image?: string, url?: string }) {
     
